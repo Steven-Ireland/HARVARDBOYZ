a77503fc70dc19541f2477a7145da9c341d881d0
Steven Ireland <spireland@wpi.edu>
2015-11-14 07:43:21 -0500

    easy commit

---
+++ index.js
@@ -0,0 +1,11 @@
+var express= require('express');
+var app = express();
+
+app.get('/', function(req, res) {
+	res.write("Hello World");
+	res.end();
+});
+
+app.listen(3000, function() {
+	console.log('listening on 3000');
+});
