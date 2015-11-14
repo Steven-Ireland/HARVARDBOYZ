bf23ad4f0fec99a00dbd8a2ae328d3b2a9577b93
Steven Ireland <spireland@wpi.edu>
2015-11-14 10:40:03 -0500

    workin basically

--- app.js
+++ app.js
@@ -1,11 +1,50 @@
 var express= require('express');
+var port = 1313;
+var io = require('socket.io')(port+1);
 var app = express();
+var http = require('https');
 
 app.get('/', function(req, res) {
 	res.send("Hello World");
 	res.end();
 });
 
-app.listen(1313, function() {
-	console.log('listening on 1313');
+io.on('connection', function(socket) {
+
 });
+
+app.listen(port, function() {
+	console.log('listening on ' + port);
+});
+
+
+function testSockets() {
+
+	var body="";
+	var req = http.request(
+		{
+			method: 'POST',
+			host: 'api.particle.io',
+			path: '/v1/devices/51ff65065067545726230187/bttn?access_token=185968afbdb45cead3dc4d17d196a9a6b822fa29'
+		},
+		function(res) {
+			console.log('requesting');
+			res.on('data', function(chunk) {
+				console.log('got chunk');
+				body+=chunk;
+			});
+			res.on('end', function() {
+
+				var json = JSON.parse(body);
+
+				console.log(json['return_value']);
+			});
+		}
+	);
+
+	req.end();
+
+
+}
+
+setInterval(testSockets, 100);
